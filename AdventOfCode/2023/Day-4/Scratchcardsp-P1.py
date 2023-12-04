b4893315303885909ec7820bac556ea478937807
Romeo <r.timmerman@id4motion.com>
2023-12-04 09:05:22 +0100

    Day 4
    Signed-off-by: Romeo <r.timmerman@id4motion.com>

---
+++ AdventOfCode/2023/Day-4/Scratchcardsp-P1.py
@@ -0,0 +1,25 @@
+with open('Cards.txt') as file:
+    lines = file.readlines()
+
+totalwin = 0
+
+for line in lines:
+    game, parts = line.split(":")
+    winningnumbers, card = parts.split("|")
+
+    winningnumbers_set = set(winningnumbers.strip().split())
+    card_set = set(card.strip().split())
+
+    winninggames = winningnumbers_set.intersection(card_set)
+
+    count_winning_numbers = len(winninggames)
+
+    if count_winning_numbers > 0:
+        calc_winning = 2 ** (count_winning_numbers - 1)
+    else:
+        calc_winning = 0
+
+    totalwin += calc_winning
+
+    print(f"Calculated: {totalwin}")
+
